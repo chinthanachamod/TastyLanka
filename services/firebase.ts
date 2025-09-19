ea5875e92678d89a6cc5461423b19b07f7029b6a
chinthanachamod <chinthanachamod50@gmail.com>
2025-09-20 00:03:22 +0530

    remove some useless codes

--- services/firebase.ts
+++ services/firebase.ts
@@ -21,26 +21,3 @@ export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
 const analytics = getAnalytics(app);
 export default app;
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBrN_jzK32PjesXFvi8pRYw6L49-7DFYps",
-//   authDomain: "tastylanka-e9de3.firebaseapp.com",
-//   projectId: "tastylanka-e9de3",
-//   storageBucket: "tastylanka-e9de3.firebasestorage.app",
-//   messagingSenderId: "261282321718",
-//   appId: "1:261282321718:web:73ed2fa96c1538cce90f97",
-//   measurementId: "G-PQRCSMD0SP"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
