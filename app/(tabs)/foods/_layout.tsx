33e6a493dc02b796eaebc0fe570548c9f17b4013
chinthanachamod <chinthanachamod50@gmail.com>
2025-09-21 18:46:11 +0530

    hide FoodDetails page header shown

--- app/(tabs)/foods/_layout.tsx
+++ app/(tabs)/foods/_layout.tsx
@@ -6,7 +6,7 @@ const FavouritesLayout = () => {
   return (
     <Stack screenOptions={{ animation: "slide_from_right" }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="[id]" options={{ title: "FoodDetails" }} />
+      <Stack.Screen name="[id]" options={{ title: "FoodDetails", headerShown: false }} />
     </Stack>
   )
 }
